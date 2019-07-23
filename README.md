96790a9b5bbe1e4ae34070e1ae92503e42d5135f
Payam <payam.emami@scilifelab.se>
2019-07-23 15:25:59 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # r-kubernetes
 A simple script to offload R sessions to Kubernetes
+
+# How it works
+It's assumed that the parent r/rstudio is running in a pod in the same cluster as Kubernetes. 
+
+You will also need to prepare your docker image which must contain all the required packages including batchtools, future.batchtools and listenv!
+
+You can for example download rocker https://github.com/rocker-org/rocker-versioned and add the following line at the end of the Dockerfile in the r subdicrectory and build it.
+
+RUN R -e 'install.packages(c("batchtools","future.batchtools","listenv"))'
+
+To run test the script you can use future.batchtools:
+
+
+library(future.batchtools)
+cluster.functions <- makeClusterFunctionsk8s(image = "payamemami/r-ver:latest",MEMORYREQ ="5G",MEMORYLIMIT = "16G")
+
+plan(batchtools_custom, cluster.functions = cluster.functions)
+library(listenv)
+results <- listenv()
+
+for (ss in 1:100) {
+  results[[ss]] %<-% {
+    princomp(matrix(rnorm(32000000), 1000000, 32))$sdev[1]
+  }
+}
+
+as.list(results)
+
+The code above runs 100 PCAs on a large dataset. 
+
+# How to provision R
+
+The easiest way is to either use Rancher or KubeNow! I used Kubenow as it provisions a GlusterFS node which makes things a lot easier!
+
+Given that you use KubeNow to deploy a cluster, you can ssh into the master e.g. kn ssh and run:
+
+    helm repo add rstudio-helm
+    https://payamemami.github.io/rstudio-helm-chart
+
+and then 
+
+    helm upgrade
+    --install
+    --set rstudio_image_registry="docker.io",rstudio_image_tag=":v1",passwd_rstudio="https://raw.githubusercontent.com/PayamEmami/tmp/master/tt.txt",use_ingress="yes",hostname="rstudio",domain="MASTERIP.nip.io",external_ingress_controller="yes",pvc_exists="yes",rstudio_pvc="{{ jupyter_pvc }}",rstudio_resource_req_cpu="{{ jupyter_resource_req_cpu }}",rstudio_resource_req_memory="{{ jupyter_resource_req_memory }}"
+    --version "0.1.0"
+    "jupyter-rstudio-0.1.0"
+    rstudio-helm/rstudio
+  no_log: "{{ nologging }}"
+
+
+MASTER ip should be the floating ip address assigned to your master passwd_rstudio is a file that contains user and passwords for the users of rstudio (this should be available through a link). The file is comma separated and should be username,password,userid,groupid,true or false (whether the user has admin premission),022
+
+
---
+++ makeClusterFunctionsk8s.r
@@ -0,0 +1,112 @@
+makeClusterFunctionsk8s = function(image, PVC="galaxy-pvc",MOUNTPATH="/home",MOUNTSUB="rstudio",
+                                   CPULIMIT="5",MEMORYLIMIT="4G",
+                                   CPUREQ="1",MEMORYREQ="1G",templatePath="jobTemplate.json",tokenPath="/var/run/secrets/kubernetes.io/serviceaccount/token") { 
+  
+
+  require("httr")
+  require("jsonlite")
+  
+  user = Sys.info()["user"]
+  
+  jobTemplate<-readLines(templatePath)
+  
+ 
+  
+  submitJob = function(reg, jc) {
+    assertRegistry(reg, writeable = TRUE)
+    JobName<-paste(user,jc$job.hash,sep = "-")
+    print(JobName)
+    req_token<-readLines(tokenPath)
+    url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
+    dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)))
+    dataTMP<-content(dataTMP)
+    availableJobs<-c()
+    if("items"%in%names(dataTMP))
+    {
+      availableJobs<-sapply(dataTMP$items,function(x){x$metadata$name}) 
+    }
+
+    JobCounter=1
+    while(JobName%in%availableJobs)
+    {
+      JobName<-paste(user,jc$job.hash,JobCounter,sep = "-")
+     
+    }
+    
+    replacementData<-data.frame(from=c("%JOBNAME%","%PVC%","%CONTAINERIMAGE%","%COMMAND%",
+                                       "%CPULIMIT%","%MEMORYLIMIT%","%CPUREQ%","%MEMORYREQ%","%MOUNTPATH%","%MOUNTSUB%"),
+to=c(JobName,"galaxy-pvc",image,
+     paste(shQuote("Rscript",type = "cmd"),shQuote("-e",type = "cmd"),
+           shQuote(sprintf("batchtools::doJobCollection('%s', '%s')", jc$uri, jc$log.file),type = "cmd"),
+           sep = ",\n"),
+     CPULIMIT,MEMORYLIMIT,CPUREQ,MEMORYREQ,MOUNTPATH,MOUNTSUB))
+    
+
+    for(i in 1:nrow(replacementData))
+    {
+      jobTemplate<-gsub(replacementData[i,"from"],replacementData[i,"to"],x = jobTemplate,fixed = T)
+    }
+    
+    
+    url="https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs"
+    tmpData <- POST(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),body = jobTemplate,encode = "json",
+                    query=list("pretty"=T))
+    tmpData<-content(tmpData)
+
+    
+    
+    if (length(tmpData$status)>0 && tmpData$status=="Failure") {
+      housekeeping(reg)
+      no.res.msg = tmpData$message
+      return(cfHandleUnknownSubmitError(jobTemplate, tmpData$message, tmpData$message))
+    }
+      return(makeSubmitJobResult(status = 0L, batch.id = JobName))
+    }
+  
+  
+  listJobs = function(reg, filter = character(0L)) {
+    assertRegistry(reg, writeable = FALSE)
+ 
+    req_token<-readLines(tokenPath)
+    url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
+    dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)))
+    dataTMP<-content(dataTMP)
+    availableJobs<-c()
+    if("items"%in%names(dataTMP))
+    {
+      availableJobs<-sapply(dataTMP$items,function(x){x$metadata$name}) 
+      availableJobs<-availableJobs[startsWith(x = availableJobs,prefix = as.character(user))]
+    }
+    return(availableJobs)
+  }
+  
+  
+  killJob = function(reg, batch.id) {
+    assertRegistry(reg, writeable = TRUE)
+    
+    req_token<-readLines(tokenPath)
+    url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs/",batch.id,sep = "")
+    dataTMP <- DELETE(url, config = add_headers(Authorization=paste0("Bearer ", req_token)))
+    
+
+  }
+  
+  listJobsRunning = function(reg) {
+    assertRegistry(reg, writeable = FALSE)
+    req_token<-readLines(tokenPath)
+    url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
+    dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)))
+    dataTMP<-content(dataTMP)
+    availableJobs<-c()
+    if("items"%in%names(dataTMP))
+    {
+      availableJobs<-sapply(dataTMP$items,function(x){if("active"%in%names(x$metadata$status)){as.character(x$metadata$name)}else{F}}) 
+      availableJobs<-availableJobs[availableJobs!=F]
+      if(length(availableJobs)==0)availableJobs<-c()
+    }
+    return(availableJobs)
+  }
+  
+  makeClusterFunctions(name = "k8s", submitJob = submitJob, killJob = killJob, listJobsRunning = listJobsRunning,
+                       store.job.collection = TRUE)
+}
