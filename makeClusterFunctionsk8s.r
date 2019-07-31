216a6ecab3e940305d5aac8ff72ffd6d953b3c79
PayamEmami <payam.emami@scilifelab.se>
2019-07-31 10:26:49 +0200

    Update makeClusterFunctionsk8s.r

--- makeClusterFunctionsk8s.r
+++ makeClusterFunctionsk8s.r
@@ -1,9 +1,9 @@
-makeClusterFunctionsk8s = function(image, PVC="galaxy-pvc",MOUNTPATH="/home",MOUNTSUB="rstudio",
+makeClusterFunctionsk8s = function(image, PVC="",MOUNTPATH="/home",MOUNTSUB="",
                                    CPULIMIT="5",MEMORYLIMIT="4G",
                                    CPUREQ="1",MEMORYREQ="1G",templatePath="jobTemplate.json",
                                    tokenPath="/var/run/secrets/kubernetes.io/serviceaccount/token",
-                                  certificatePath="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt") { 
-
+                                   certificatePath="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt") { 
+  
   require("httr")
   require("jsonlite")
   
@@ -11,7 +11,7 @@ makeClusterFunctionsk8s = function(image, PVC="galaxy-pvc",MOUNTPATH="/home",MOU
   
   jobTemplate<-readLines(templatePath)
   
- 
+  
   
   submitJob = function(reg, jc) {
     assertRegistry(reg, writeable = TRUE)
@@ -20,30 +20,30 @@ makeClusterFunctionsk8s = function(image, PVC="galaxy-pvc",MOUNTPATH="/home",MOU
     req_token<-readLines(tokenPath)
     url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
     dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),
-                  config(cainfo=certificatePath))
+                   config(cainfo=certificatePath))
     dataTMP<-content(dataTMP)
     availableJobs<-c()
     if("items"%in%names(dataTMP))
     {
       availableJobs<-sapply(dataTMP$items,function(x){x$metadata$name}) 
     }
-
+    
     JobCounter=1
     while(JobName%in%availableJobs)
     {
       JobName<-paste(user,jc$job.hash,JobCounter,sep = "-")
-     
+      
     }
     
     replacementData<-data.frame(from=c("%JOBNAME%","%PVC%","%CONTAINERIMAGE%","%COMMAND%",
                                        "%CPULIMIT%","%MEMORYLIMIT%","%CPUREQ%","%MEMORYREQ%","%MOUNTPATH%","%MOUNTSUB%"),
-to=c(JobName,"galaxy-pvc",image,
-     paste(shQuote("Rscript",type = "cmd"),shQuote("-e",type = "cmd"),
-           shQuote(sprintf("batchtools::doJobCollection('%s', '%s')", jc$uri, jc$log.file),type = "cmd"),
-           sep = ",\n"),
-     CPULIMIT,MEMORYLIMIT,CPUREQ,MEMORYREQ,MOUNTPATH,MOUNTSUB))
+                                to=c(JobName,PVC,image,
+                                     paste(shQuote("Rscript",type = "cmd"),shQuote("-e",type = "cmd"),
+                                           shQuote(sprintf("batchtools::doJobCollection('%s', '%s')", jc$uri, jc$log.file),type = "cmd"),
+                                           sep = ",\n"),
+                                     CPULIMIT,MEMORYLIMIT,CPUREQ,MEMORYREQ,MOUNTPATH,MOUNTSUB))
     
-
+    
     for(i in 1:nrow(replacementData))
     {
       jobTemplate<-gsub(replacementData[i,"from"],replacementData[i,"to"],x = jobTemplate,fixed = T)
@@ -53,27 +53,26 @@ to=c(JobName,"galaxy-pvc",image,
     url="https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs"
     tmpData <- POST(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),body = jobTemplate,encode = "json",
                     query=list("pretty"=T),
-                   config(cainfo=certificatePath))
+                    config(cainfo=certificatePath))
     tmpData<-content(tmpData)
-
+    
     
     
     if (length(tmpData$status)>0 && tmpData$status=="Failure") {
-      housekeeping(reg)
       no.res.msg = tmpData$message
-      return(cfHandleUnknownSubmitError(jobTemplate, tmpData$message, tmpData$message))
+      return(cfHandleUnknownSubmitError(jobTemplate, 1, tmpData$message))
     }
-      return(makeSubmitJobResult(status = 0L, batch.id = JobName))
-    }
+    return(makeSubmitJobResult(status = 0L, batch.id = JobName))
+  }
   
   
   listJobs = function(reg, filter = character(0L)) {
     assertRegistry(reg, writeable = FALSE)
- 
+    
     req_token<-readLines(tokenPath)
     url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
     dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),
-                  config(cainfo=certificatePath))
+                   config(cainfo=certificatePath))
     dataTMP<-content(dataTMP)
     availableJobs<-c()
     if("items"%in%names(dataTMP))
@@ -91,9 +90,9 @@ to=c(JobName,"galaxy-pvc",image,
     req_token<-readLines(tokenPath)
     url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs/",batch.id,sep = "")
     dataTMP <- DELETE(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),
-                     config(cainfo=certificatePath))
+                      config(cainfo=certificatePath))
     
-
+    
   }
   
   listJobsRunning = function(reg) {
@@ -101,7 +100,7 @@ to=c(JobName,"galaxy-pvc",image,
     req_token<-readLines(tokenPath)
     url=paste("https://kubernetes.default.svc.cluster.local/apis/batch/v1/namespaces/default/jobs",sep = "")
     dataTMP <- GET(url, config = add_headers(Authorization=paste0("Bearer ", req_token)),
-                  config(cainfo=certificatePath))
+                   config(cainfo=certificatePath))
     dataTMP<-content(dataTMP)
     availableJobs<-c()
     if("items"%in%names(dataTMP))
